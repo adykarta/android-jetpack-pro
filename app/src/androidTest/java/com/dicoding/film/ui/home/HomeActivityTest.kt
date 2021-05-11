2d23be46eb5c88a8ce941680d854faa76bb1f1f5
Muhamad Istiady Kartadibrata <mkartadibrata@gmail.com>
2021-05-11 09:19:07 +0700

    fixed instrument test

--- app/src/androidTest/java/com/dicoding/film/ui/home/HomeActivityTest.kt
+++ app/src/androidTest/java/com/dicoding/film/ui/home/HomeActivityTest.kt
@@ -1,15 +1,21 @@
 package com.dicoding.film.ui.home
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.NoMatchingViewException
+import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.pressBack
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import com.dicoding.film.R
 import com.dicoding.film.utils.EspressoIdlingResource
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.Matcher
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -71,17 +77,52 @@ class HomeActivityTest{
 
     @Test
     fun loadFavoriteFilm(){
+        onView(withId(R.id.rv_film)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.action_favorite)).perform(click())
+        onView(isRoot()).perform(pressBack())
         onView(withId(R.id.fav)).perform(click())
         onView(withId(R.id.rv_film_favorite)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_film_favorite)).check(RecyclerViewItemCountAssertion(1));
+        onView(isRoot()).perform(pressBack())
+        onView(withId(R.id.rv_film)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.action_favorite)).perform(click())
+        onView(isRoot()).perform(pressBack())
     }
 
     @Test
     fun loadFavoriteTv(){
+        onView(withText("Tv Shows")).perform(click())
+        onView(withId(R.id.rv_tvshow)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.action_favorite)).perform(click())
+        onView(isRoot()).perform(pressBack())
         onView(withId(R.id.fav)).perform(click())
         onView(withText("Tv Shows")).perform(click())
         onView(withId(R.id.rv_tvshow_favorite)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_tvshow_favorite)).check(RecyclerViewItemCountAssertion(1))
+        onView(isRoot()).perform(pressBack())
+        onView(withText("Tv Shows")).perform(click())
+        onView(withId(R.id.rv_tvshow)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.action_favorite)).perform(click())
+        onView(isRoot()).perform(pressBack())
     }
 
 
 
+}
+
+class RecyclerViewItemCountAssertion : ViewAssertion {
+    private val matcher: Matcher<Int?>?
+
+    constructor(expectedCount: Int) {
+        matcher = `is`(expectedCount)
+    }
+
+    override fun check(view: View?, noViewFoundException: NoMatchingViewException?) {
+        if (noViewFoundException != null) {
+            throw noViewFoundException
+        }
+        val recyclerView = view as RecyclerView?
+        val adapter = recyclerView!!.adapter
+        assertThat(adapter!!.itemCount, matcher)
+    }
 }
